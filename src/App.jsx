504e219b0816512d8b92baec9aef6afd03383521
alexanderliberacion-cmd <alexanderliberacion@gmail.com>
2026-05-29 11:28:28 +0200

    mejora del diseño para moviles

--- src/App.jsx
+++ src/App.jsx
@@ -47,7 +47,7 @@ export const App = () => {
     //La weathercard recibe la informacion solamente si weatherData tiene datos
     return (
         <main className="bg-slate-900  flex flex-col justify-center min-w-screen min-h-screen items-center">
-            <div id="width" className="max-w-7xl md:max-w-2xl mx-auto w-full px-4">
+            <div id="width" className="max-w-7xl md:max-w-2xl mx-auto px-4">
                 <h1 className="text-3xl font-bold text-white mb-4">Weather App</h1>
                 <section>
                     <SearchBar onCityChange={cityCallback}/>
